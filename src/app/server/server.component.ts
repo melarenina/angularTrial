369500515393810428c48ef81e9dfb4a746fcf24
Marina Melaré <melare.nina@gmail.com>
2020-04-02 19:58:18 -0300

    Style Elements Dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,7 +9,16 @@ export class ServerComponent{
     serverId = 10;
     serverStatus = 'offline';
 
+    constructor(){
+        //If the number generated is bigger than 0.5, set the status as online. Otherwise, set as offline.
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getServerStatus(){
         return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red'
     }
 }
